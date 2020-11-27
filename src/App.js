7f9e57192f0e7f248df9a265a6e24880a8b67108
lady pino <ladypino112@gmail.com>
2020-11-27 00:43:57 -0300

    landing changes

--- src/App.js
+++ src/App.js
@@ -6,23 +6,38 @@ import LandingPage from './pages/LandingPage';
 import Traceability from './pages/Traceability';
 import Statistics from './pages/statistics.js';
 
+import React, { useState } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import LandingPage from './pages/landingPage'
+import DonationPage from './pages/donationPage';
+import AboutUs from './pages/aboutUsPage';
+// import DataGridDemo from './components/intranetData/intranetTable';
+import IntranetPage from './pages/intranet';
+import Login from './components/demo/login'
+import Sidebar from './pages/sidebar';
+
+export const ShowContext = React.createContext(null);
 
 function App() {
-
+  const [formToShow, setFormToShow] = useState('Overview');
+  const [positionScrollForm, setPositionScrollForm] = useState(0);
 
   return (
     <Router>
       <Switch>
-        <Route path="/Traceability" exact >
-        <Traceability />
+        <Route path="/Overview" exact >
+          <ShowContext.Provider
+            value={{
+              formToShow: [formToShow, setFormToShow],
+              positionScrollForm: [positionScrollForm, setPositionScrollForm]
+            }}
+          >
+            <Sidebar/>
+          </ShowContext.Provider>
         </Route>
-
-         <Route path="/Statistics" exact >
-            <Statistics />
-         </Route>
-         
         
-       
+        
       </Switch>
     </Router >
   );
--- src/pages/sidebar.js
+++ src/pages/sidebar.js
@@ -1,45 +1,73 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+
+
 import './Sidebar.css';
 import Traceability from './Traceability.js'
+import Statistics from './statistics.js'
+
+import React, { useContext, useEffect } from 'react';
+
+import { ShowContext } from '../../App'
+
+import { animateScroll as scroll } from 'react-scroll';
 
 
-const Sidebar= (props) => {
-  
+function Sidebar() {
+
+  const scrollForm = window.screen.width < 1024 ? 390 : 780;
+
+  // me traigo valores del contexto
+  const { formToShow, positionScrollForm } = useContext(ShowContext);
+
+  // le doy un valor a los estados de cada objeto
+  const [formToShowValue, setFormToShowValue] = formToShow;
+  const [positionScrollFormValue, setPositionScrollFormValue] = positionScrollForm;
+
+  useEffect(() => {
+    scroll.scrollTo(positionScrollFormValue)
+  }
+  )
+
+  const classButtonArchiveOVER = formToShowValue === 'Overview' ? 'archiveBtnOn' : 'archiveBtnOff';
+  const classButtonArchiveTick = formToShowValue === 'Tickets' ? 'archiveBtnOn' : 'archiveBtnOff';
+  const classButtonArchiveTEAMS = formToShowValue === 'Teams' ? 'archiveBtnOn' : 'archiveBtnOff';
+  const classButtonArchiveSETT = formToShowValue === 'Settings' ? 'archiveBtnOn' : 'archiveBtnOff';
+  const classButtonArchiveLOG = formToShowValue === 'Logout' ? 'archiveBtnOn' : 'archiveBtnOff';
+
+  let form = '';
+
+  if (formToShowValue === 'Overview') form = <Statistics/>
+  if (formToShowValue === 'Tickets') form = <Traceability />
+  if (formToShowValue === 'Teams') form = <LandingPage />
+  if (formToShowValue === 'Settings') form = <LandingPage />
+  if (formToShowValue === 'Logout') form = <LandingPage />
+
   return (
-    <div className='containerBurguerToggle'>
-      <div className='boxSection'>
-        <Link to="/Traceability" className='oli'>
-          <div className='boxSection'>
-            <p className='textBurgerToggle'
-           >Overview</p>
-          </div>
-        </Link>
-        <Link to="/Statistics" className='oli'>
-          <div className='boxSection'>
-            
-            <p className='textBurgerToggle'>Tickets</p>
-          </div>
-        </Link>
-        <Link to="/Statistics" className='oli'>
-          <div className='boxSection'
->
-            <p className='textBurgerToggle'>Team</p>
-          </div>
-        </Link>
-        <Link to="/" className='oli'>
-          <div className='boxSection'>
-            <p className='textBurgerToggle'
-         >Setting</p>
-          </div>
-        </Link>
-        <Link to="/Donaciones" className='oli'>
-          <div className='boxSection'>
-            <p className='textBurgerToggle'
-          >Logout</p>
-          </div>
-        </Link>
+    <div className='containerSectionArchiveBtnAndDonateForms'>
+      <div className='sectionArchiveButtons'>
+        <button className={classButtonArchiveOVER}
+          onClick={() => {
+            setFormToShowValue('Overview');
+            setPositionScrollFormValue(scrollForm);
+          }}>NDA</button>
+        <button className={classButtonArchiveTick}
+          onClick={() => {
+            setFormToShowValue('Tickets');
+            setPositionScrollFormValue(scrollForm)
+          }}>CIBERSEGURIDAD</button>
+        <button className={classButtonArchiveTEAMS} onClick={() => {
+          setFormToShowValue('Teams');
+          setPositionScrollFormValue(scrollForm)
+        }}>LBA</button>
+        <button className={classButtonArchiveSETT} onClick={() => {
+          setFormToShowValue('Settings');
+          setPositionScrollFormValue(scrollForm);
+        }}>BHP</button>
+        <button className={classButtonArchiveLOG} onClick={() => {
+          setFormToShowValue('Logout');
+          setPositionScrollFormValue(scrollForm);
+        }}>ENEL</button>
       </div>
+      {form}
     </div>
   );
 }
